3c341dc9bc2d5034351e99d9c40534dafa47ec6e
Odd E. Ebbesen <oddebb@gmail.com>
2023-10-21 11:49:27 +0200

    Commented out spec for none-ls

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,16 +1,18 @@
-return {
-  {
-    "nvimtools/none-ls.nvim",
-    opts = function()
-      local nls = require("null-ls")
-      return {
-        root_dir = require("null-ls.utils").root_pattern(".null-ls-root", ".neoconf.json", "Makefile", ".git"),
-        sources = {
-          nls.builtins.formatting.stylua,
-          nls.builtins.formatting.shfmt,
-          nls.builtins.diagnostics.golangci_lint,
-        },
-      }
-    end
-  },
-}
+return {}
+-- return {
+--   {
+--     "nvimtools/none-ls.nvim",
+--     enabled = false,
+--     opts = function()
+--       local nls = require("null-ls")
+--       return {
+--         root_dir = require("null-ls.utils").root_pattern(".null-ls-root", ".neoconf.json", "Makefile", ".git"),
+--         sources = {
+--           nls.builtins.formatting.stylua,
+--           nls.builtins.formatting.shfmt,
+--           nls.builtins.diagnostics.golangci_lint,
+--         },
+--       }
+--     end,
+--   },
+-- }
