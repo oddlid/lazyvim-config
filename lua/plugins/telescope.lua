b635f820ccab2c341d84a89d50d4ba130fd5c3ff
Odd Eivind Ebbesen <odd.eivind.ebbesen@ericsson.com>
2024-01-04 09:39:42 +0100

    Add telescope function to find files in current commit
    Change-Id: Ief431e5961f954d67430bd47b737a9d91ffddf35

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,5 +1,6 @@
 return {
   -- add telescope-fzf-native
+  -- add custom find function
   {
     "telescope.nvim",
     dependencies = {
@@ -8,6 +9,15 @@ return {
       config = function()
         require("telescope").load_extension("fzf")
       end,
+    },
+    keys = {
+      {
+        "<leader>fC",
+        function()
+          require("telescope.builtin").git_files({ git_command = { "git", "diff", "--name-only", "HEAD^" } })
+        end,
+        desc = "Find in Commit",
+      },
     },
   },
 }
