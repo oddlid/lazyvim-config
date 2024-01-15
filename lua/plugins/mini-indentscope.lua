19c75431c218d01c9b930d4fdf54ec5a41570c8e
Odd E. Ebbesen <oddebb@gmail.com>
2024-01-15 22:03:34 +0100

    Changed indent guides
    - Disabled indent-blankline.nvim
    - Disabled animation for mini.indentscope.nvim
    - Set vim.opts.list = false

---
+++ lua/plugins/mini-indentscope.lua
@@ -0,0 +1,11 @@
+return {
+  {
+    "echasnovski/mini.indentscope",
+    enabled = true,
+    opts = {
+      draw = {
+        animation = require("mini.indentscope").gen_animation.none(),
+      },
+    },
+  },
+}
