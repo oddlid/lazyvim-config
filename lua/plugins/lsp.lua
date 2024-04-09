fbc6d961c4a2942a4a045b0ab891c61c88649d17
Odd Eivind Ebbesen <odd.eivind.ebbesen@ericsson.com>
2024-04-09 06:34:15 +0200

    Setup for Golang without go.nvim
    Tried to make Go tools already installed not be installed by Mason,
    which halfway works, but I get warnings, so needs to be more looked
    into.
    
    Change-Id: I41d0499a7f2eb12edb06f74383ff6c0d43623224

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -10,6 +10,46 @@ return {
           filetypes = { "go", "gomod" },
         },
       },
+      gopls = {
+        mason = false,
+        enabled = true,
+        settings = {
+          cmd = { "/home/eebbodd/go/bin/gopls" },
+          filetypes = { "go", "gomod", "gowork", "gotmpl" },
+        },
+      },
+      goimports = {
+        mason = false,
+        enabled = true,
+        settings = {
+          cmd = { "/home/eebbodd/go/bin/goimports" },
+          filetypes = { "go", "gomod", "gowork", "gotmpl" },
+        },
+      },
+      gofumpt = {
+        mason = false,
+        enabled = true,
+        settings = {
+          cmd = { "/home/eebbodd/go/bin/gofumpt" },
+          filetypes = { "go", "gomod", "gowork", "gotmpl" },
+        },
+      },
+      impl = {
+        mason = false,
+        enabled = true,
+        settings = {
+          cmd = { "/home/eebbodd/go/bin/impl" },
+          filetypes = { "go", "gomod", "gowork", "gotmpl" },
+        },
+      },
+      gomodifytags = {
+        mason = false,
+        enabled = true,
+        settings = {
+          cmd = { "/home/eebbodd/go/bin/gomodifytags" },
+          filetypes = { "go", "gomod", "gowork", "gotmpl" },
+        },
+      },
     },
   },
 }
