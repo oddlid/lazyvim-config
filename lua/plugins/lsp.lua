50bff4d7b727568f3403fbd879cfeaa2e92142fd
Odd Eivind Ebbesen <odd.eivind.ebbesen@ericsson.com>
2024-04-22 09:11:34 +0200

    Attempts at customizing gopls
    Change-Id: Ie67f3d220872fa03a965ce5391fabce539ed186e

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -14,42 +14,55 @@ return {
         mason = false,
         enabled = true,
         settings = {
-          cmd = { "/home/eebbodd/go/bin/gopls" },
+          -- init_options = {
+          --   command = {
+          --     "/tmp/gopls",
+          --     "-remote=auto",
+          --   },
+          -- },
+          -- cmd = {
+          --   "/tmp/gopls",
+          --   "-remote=auto",
+          -- },
           filetypes = { "go", "gomod", "gowork", "gotmpl" },
         },
       },
-      goimports = {
-        mason = false,
-        enabled = true,
-        settings = {
-          cmd = { "/home/eebbodd/go/bin/goimports" },
-          filetypes = { "go", "gomod", "gowork", "gotmpl" },
-        },
-      },
-      gofumpt = {
-        mason = false,
-        enabled = true,
-        settings = {
-          cmd = { "/home/eebbodd/go/bin/gofumpt" },
-          filetypes = { "go", "gomod", "gowork", "gotmpl" },
-        },
-      },
-      impl = {
-        mason = false,
-        enabled = true,
-        settings = {
-          cmd = { "/home/eebbodd/go/bin/impl" },
-          filetypes = { "go", "gomod", "gowork", "gotmpl" },
-        },
-      },
-      gomodifytags = {
-        mason = false,
-        enabled = true,
-        settings = {
-          cmd = { "/home/eebbodd/go/bin/gomodifytags" },
-          filetypes = { "go", "gomod", "gowork", "gotmpl" },
-        },
-      },
+      -- This doesn't work, since nvim-lspconfig don't have definitions matching.
+      -- I'll need undo the effect of vim.list_extend(opts.ensure_installed, { "goimports", "gofumpt", "..." })
+      -- done by LazyVim, in some way...
+      --
+      -- goimports = {
+      --   mason = false,
+      --   enabled = true,
+      --   settings = {
+      --     cmd = { "/home/eebbodd/go/bin/goimports" },
+      --     filetypes = { "go", "gomod", "gowork", "gotmpl" },
+      --   },
+      -- },
+      -- gofumpt = {
+      --   mason = false,
+      --   enabled = true,
+      --   settings = {
+      --     cmd = { "/home/eebbodd/go/bin/gofumpt" },
+      --     filetypes = { "go", "gomod", "gowork", "gotmpl" },
+      --   },
+      -- },
+      -- impl = {
+      --   mason = false,
+      --   enabled = true,
+      --   settings = {
+      --     cmd = { "/home/eebbodd/go/bin/impl" },
+      --     filetypes = { "go", "gomod", "gowork", "gotmpl" },
+      --   },
+      -- },
+      -- gomodifytags = {
+      --   mason = false,
+      --   enabled = true,
+      --   settings = {
+      --     cmd = { "/home/eebbodd/go/bin/gomodifytags" },
+      --     filetypes = { "go", "gomod", "gowork", "gotmpl" },
+      --   },
+      -- },
     },
   },
 }
