7b7fc24ca29c07cda5dc7a3ab7b5a9a4cbd86afd
Odd Eivind Ebbesen <odd.eivind.ebbesen@ericsson.com>
2024-05-23 08:45:34 +0200

    Commented out settings for indent-blankline
    Change-Id: Id4428b402e5a269b1083911e7ed13ef89a46c405

--- lua/plugins/indent-blankline.lua
+++ lua/plugins/indent-blankline.lua
@@ -1,6 +1,7 @@
-return {
-  {
-    "lukas-reineke/indent-blankline.nvim",
-    enabled = false,
-  },
-}
+return {}
+-- return {
+--   {
+--     "lukas-reineke/indent-blankline.nvim",
+--     enabled = false,
+--   },
+-- }
