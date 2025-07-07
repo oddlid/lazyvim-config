18106c4fdb30f9027afcaf7daecb2cbfd733dcc5
Odd E. Ebbesen <git@oddware.net>
2025-07-07 17:06:17 +0200

    Plugin updates, and theme config

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,3 +2,8 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 vim.opt.list = false
+vim.g.solarized_termtrans = 0
+
+-- Snacks animations
+-- Set to `false` to globally disable all snacks animations
+vim.g.snacks_animate = false
